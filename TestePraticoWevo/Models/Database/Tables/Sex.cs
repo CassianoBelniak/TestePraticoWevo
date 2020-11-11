676b1a2d299535ebef58487bd70a01c424115899
cassiano belniak <cassi.belniak@gmail.com>
2020-11-11 17:39:12 -0300

    Added sql tables

---
+++ TestePraticoWevo/Models/Database/Tables/Sex.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestePraticoWevo.Models.Database.Tables
+{
+    public class Sex
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+        public string Description { set; get; }
+    }
+}
---
+++ TestePraticoWevo/Models/Database/Tables/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestePraticoWevo.Models.Database.Tables
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public string CPF { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public Sex Sex { set; get; }
+        public DateTime Birthday { set; get; }
+    }
+}
